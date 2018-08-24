d33f7b7581437a1d70f51e62b4ed794a344ca029
Bhavin Patel <bpatel@caps.ua.edu>
2018-08-24 15:55:09 -0500

    adding few more ui

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { ThemeService } from "../_services/theme/theme.service";
+import { ActionSheetController, AlertController } from "@ionic/angular";
 
 const themes = {
   autumn: {
@@ -34,7 +35,85 @@ const themes = {
   styleUrls: ["home.page.scss"]
 })
 export class HomePage {
-  constructor(private theme: ThemeService) {}
+  constructor(
+    private theme: ThemeService,
+    public actionSheetController: ActionSheetController,
+    public alertController: AlertController
+  ) {}
+
+  // presentBasic() {
+  //   return await presentActionSheet();
+  // }
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: "Albums",
+      buttons: [
+        {
+          text: "Delete",
+          role: "destructive",
+          icon: "trash",
+          handler: () => {
+            console.log("Delete clicked");
+          }
+        },
+        {
+          text: "Share",
+          icon: "share",
+          handler: () => {
+            console.log("Share clicked");
+          }
+        },
+        {
+          text: "Play (open modal)",
+          icon: "arrow-dropright-circle",
+          handler: () => {
+            console.log("Play clicked");
+          }
+        },
+        {
+          text: "Favorite",
+          icon: "heart",
+          handler: () => {
+            console.log("Favorite clicked");
+          }
+        },
+        {
+          text: "Cancel",
+          icon: "close",
+          role: "cancel",
+          handler: () => {
+            console.log("Cancel clicked");
+          }
+        }
+      ]
+    });
+    await actionSheet.present();
+  }
+
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      header: "Confirm!",
+      message: "Message <strong>text</strong>!!!",
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+          cssClass: "secondary",
+          handler: blah => {
+            console.log("Confirm Cancel: blah");
+          }
+        },
+        {
+          text: "Okay",
+          handler: () => {
+            console.log("Confirm Okay");
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 
   changeTheme(name) {
     this.theme.setTheme(themes[name]);
