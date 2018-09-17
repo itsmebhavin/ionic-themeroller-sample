2e70e9c7a3c450d23eaa667e1bb0d0b8f1f1b8be
Bhavin Patel <bpatel@caps.ua.edu>
2018-09-17 15:29:46 -0500

    just few PWA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,16 @@ import { Platform } from "@ionic/angular";
 import { SplashScreen } from "@ionic-native/splash-screen/ngx";
 import { StatusBar } from "@ionic-native/status-bar/ngx";
 import { ThemeService } from "./_services/theme/theme.service";
+import { SwUpdate } from "@angular/service-worker";
+import { ToastController } from "@ionic/angular";
+import { environment } from "../environments/environment";
 @Component({
   selector: "app-root",
   templateUrl: "app.component.html"
 })
 export class AppComponent implements OnInit {
+  currentTheme;
+  update = false;
   public appPages = [
     {
       title: "Home",
@@ -25,11 +30,47 @@ export class AppComponent implements OnInit {
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private theme: ThemeService
+    private theme: ThemeService,
+    private updates: SwUpdate,
+    private toastController: ToastController
   ) {
+    updates.available.subscribe(event => {
+      console.log("current version is", event.current);
+      console.log("available version is", event.available);
+      this.update = true;
+
+      // Watch Video -https://www.youtube.com/watch?v=othhfZ0mGjU
+      // Another article - https://alligator.io/angular/service-workers/
+      // You can do this as well, instead of showing toast to user and setting flag true / false
+      //     but the problem is - lazy loading won't work then. Please check - https://angular.io/guide/service-worker-communications
+      // Doing this could break lazy-loading into currently running apps,
+      //      especially if the lazy-loaded chunks use filenames with hashes, which change every version.
+      if (environment.production) {
+        this.updates.activateUpdate().then(() => {
+          location.reload(true);
+        });
+      }
+      this.presentToastWithOptions();
+    });
+    updates.activated.subscribe(event => {
+      console.log("old version was", event.previous);
+      console.log("new version is", event.current);
+    });
     this.initializeApp();
   }
-
+  async presentToastWithOptions() {
+    const toast = await this.toastController.create({
+      message: "We have published a new release!",
+      showCloseButton: true,
+      position: "top",
+      closeButtonText: "Activate & Reload"
+    });
+    toast.dismiss(() => {
+      console.log("Ion-Toast is dismissed");
+      document.location.reload();
+    });
+    toast.present();
+  }
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
@@ -37,17 +78,18 @@ export class AppComponent implements OnInit {
     });
   }
 
-  currentTheme;
   ngOnInit() {
     this.setGlobalTheme();
-    this.theme.onThemeChanged.subscribe(x => { this.themeChange(x) });
+    this.theme.onThemeChanged.subscribe(x => {
+      this.themeChange(x);
+    });
   }
   async setGlobalTheme() {
     this.currentTheme = await this.theme.storedThemeName.then(x => x);
     console.log(this.currentTheme);
-    this.changeTheme(this.currentTheme)
+    this.changeTheme(this.currentTheme);
   }
-  changeTheme(name = '') {
+  changeTheme(name = "") {
     this.theme.setThemeByName(name);
   }
   themeChange($event) {
