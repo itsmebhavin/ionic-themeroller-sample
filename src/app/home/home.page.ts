fadc14eb60ec850810a45ba6ba169d4c6619b3ec
CAPS\YSero <yuri.sero@ua.edu>
2018-09-20 11:49:54 -0500

    Implemented barcode scanner as previously discussed

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,103 +1,111 @@
 import { Component, OnInit } from "@angular/core";
 import { ActionSheetController, AlertController } from "@ionic/angular";
 import { TodoService } from "../_services/todo/todo.service";
-import { Router } from "@angular/router";
+import { Router, ActivatedRoute } from "@angular/router";
 import { Storage } from "@ionic/storage";
 import { ThemeService } from "../_services/theme/theme.service";
 
 
 @Component({
-  selector: "app-home",
-  templateUrl: "home.page.html",
-  styleUrls: ["home.page.scss"]
+	selector: "app-home",
+	templateUrl: "home.page.html",
+	styleUrls: ["home.page.scss"]
 })
-export class HomePage{
-  constructor(
-    private theme: ThemeService,
-    public actionSheetController: ActionSheetController,
-    public alertController: AlertController,
-    public todoService: TodoService,
-    public router: Router
-  ) {}
- 
+export class HomePage {
+	barcode: string;
 
-  async presentActionSheet() {
-    const actionSheet = await this.actionSheetController.create({
-      header: "Albums",
-      buttons: [
-        {
-          text: "Delete",
-          role: "destructive",
-          icon: "trash",
-          handler: () => {
-            console.log("Delete clicked");
-          }
-        },
-        {
-          text: "Share",
-          icon: "share",
-          handler: () => {
-            console.log("Share clicked");
-          }
-        },
-        {
-          text: "Play (open modal)",
-          icon: "arrow-dropright-circle",
-          handler: () => {
-            console.log("Play clicked");
-          }
-        },
-        {
-          text: "Favorite",
-          icon: "heart",
-          handler: () => {
-            console.log("Favorite clicked");
-          }
-        },
-        {
-          text: "Cancel",
-          icon: "close",
-          role: "cancel",
-          handler: () => {
-            console.log("Cancel clicked");
-          }
-        }
-      ]
-    });
-    await actionSheet.present();
-  }
+	constructor(
+		private theme: ThemeService,
+		public actionSheetController: ActionSheetController,
+		public alertController: AlertController,
+		public todoService: TodoService,
+		public router: Router,
+		private route: ActivatedRoute
+	) { }
 
-  async presentAlertConfirm() {
-    const alert = await this.alertController.create({
-      header: "Confirm!",
-      message: "Message <strong>text</strong>!!!",
-      buttons: [
-        {
-          text: "Cancel",
-          role: "cancel",
-          cssClass: "secondary",
-          handler: blah => {
-            console.log("Confirm Cancel: blah");
-          }
-        },
-        {
-          text: "Okay",
-          handler: () => {
-            console.log("Confirm Okay");
-          }
-        }
-      ]
-    });
+	ngOnInit() {
+		this.route.params.subscribe(params => {
+			this.barcode = params.queryParams;
+		});
+	}
 
-    await alert.present();
-  }
+	async presentActionSheet() {
+		const actionSheet = await this.actionSheetController.create({
+			header: "Albums",
+			buttons: [
+				{
+					text: "Delete",
+					role: "destructive",
+					icon: "trash",
+					handler: () => {
+						console.log("Delete clicked");
+					}
+				},
+				{
+					text: "Share",
+					icon: "share",
+					handler: () => {
+						console.log("Share clicked");
+					}
+				},
+				{
+					text: "Play (open modal)",
+					icon: "arrow-dropright-circle",
+					handler: () => {
+						console.log("Play clicked");
+					}
+				},
+				{
+					text: "Favorite",
+					icon: "heart",
+					handler: () => {
+						console.log("Favorite clicked");
+					}
+				},
+				{
+					text: "Cancel",
+					icon: "close",
+					role: "cancel",
+					handler: () => {
+						console.log("Cancel clicked");
+					}
+				}
+			]
+		});
+		await actionSheet.present();
+	}
 
-  changeTheme(name = '') {
-    this.theme.setThemeByName(name);
-    this.theme.onThemeChanged.emit(name);
-  }
+	async presentAlertConfirm() {
+		const alert = await this.alertController.create({
+			header: "Confirm!",
+			message: "Message <strong>text</strong>!!!",
+			buttons: [
+				{
+					text: "Cancel",
+					role: "cancel",
+					cssClass: "secondary",
+					handler: blah => {
+						console.log("Confirm Cancel: blah");
+					}
+				},
+				{
+					text: "Okay",
+					handler: () => {
+						console.log("Confirm Okay");
+					}
+				}
+			]
+		});
 
-  searchBarcode() {
-    this.router.navigate(["barcode"]);
-  }
+		await alert.present();
+	}
+
+	changeTheme(name = '') {
+		this.theme.setThemeByName(name);
+		this.theme.onThemeChanged.emit(name);
+	}
+
+	searchBarcode() {
+		this.router.navigate(["barcode"]);
+	}
 }
